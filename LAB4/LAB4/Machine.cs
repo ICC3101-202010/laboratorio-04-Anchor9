425de66126e342e0c215fc61bcd494126e56878e
unknown <fjmiranda4iuandes.cl>
2020-04-18 18:14:16 -0400

    primero

---
+++ LAB4/LAB4/Machine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4
+{
+    public abstract class Machine
+    {
+        private string name;
+        public abstract void TurnOff();
+        public abstract void TurnOn();
+        public abstract void Reboot();
+    }
+
+
+}
