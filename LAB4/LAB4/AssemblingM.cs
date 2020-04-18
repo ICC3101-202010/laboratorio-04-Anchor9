a0d99761f1bd7111ad49761e5d2e881dd21fb360
unknown <fjmiranda4iuandes.cl>
2020-04-18 19:46:53 -0400

    assembly structure

--- LAB4/LAB4/AssemblingM.cs
+++ LAB4/LAB4/AssemblingM.cs
@@ -10,6 +10,45 @@ namespace LAB4
     {
         private List<Part> parts;
 
+        public AssemblingM()
+        {
+        }
+
+        public void AddPartsAssemblyList(Part part, List<Part> parts)
+        {
+            int weight = part.Weight;
+            int height = part.Height;
+            string brand = part.Brand;
+            int width = part.Width;
+            string material = part.Material;
+            string type = part.Type;
+
+            parts.Add(new Part(weight, height, width, type, brand, material));
+        }
+
+        public string AcheckMemory(List<Part> parts) 
+        {
+            int parts_quantity = parts.Count();
+            string answer;
+
+            if (parts_quantity >= 7)
+            {
+                Console.WriteLine("Assembly memory full, please reboot Machine");
+                answer = "yes";
+                return answer;
+            }
+            else 
+            {
+                Console.WriteLine("Memory not yet full");
+                answer = "no";
+                return answer;
+            } 
+        }
+        public void AEmptyMemory(List<Part> parts) 
+        {
+            parts.Clear();
+        }
+
     }
 
 }
